6107668b6bd3d8dced8a78498938e071cabbce87
aldia <aldialiu@lilith.com>
2022-02-15 17:42:36 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kyicy/pre-commit-goreportcard
+
+go 1.17
+
+require github.com/gojp/goreportcard v0.0.0-20220211091849-e4ed167d863e
---
+++ main/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gojp/goreportcard/check"
+)
+
+var (
+	dir     = flag.String("d", ".", "Root directory of your Go application")
+	verbose = flag.Bool("v", false, "Verbose output")
+	th      = flag.Float64("t", 90, "Threshold of failure command")
+)
+
+func main() {
+	flag.Parse()
+
+	result, err := check.Run(*dir)
+	if err != nil {
+		log.Fatalf("Fatal error checking %s: %s", *dir, err.Error())
+	}
+
+	fmt.Printf("Grade: %s (%.1f%%)\n", result.Grade, result.Average*100)
+	fmt.Printf("Files: %d\n", result.Files)
+	fmt.Printf("Issues: %d\n", result.Issues)
+
+	for _, c := range result.Checks {
+		fmt.Printf("%s: %d%%\n", c.Name, int64(c.Percentage*100))
+		if *verbose && len(c.FileSummaries) > 0 {
+			for _, f := range c.FileSummaries {
+				fmt.Printf("\t%s\n", f.Filename)
+				for _, e := range f.Errors {
+					fmt.Printf("\t\tLine %d: %s\n", e.LineNumber, e.ErrorString)
+				}
+			}
+		}
+	}
+
+	if result.Average*100 < *th {
+		os.Exit(1)
+	}
+}
