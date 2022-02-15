c0b2a82d37e0c2b013c442aa7c93ada3e7a4de41
aldia <aldialiu@lilith.com>
2022-02-15 17:56:56 +0800

    customize checks

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pre-commit-goreportcard
 golang https://github.com/gojp/goreportcard hook for http://pre-commit.com/
+
+
+Add this to your .pre-commit-config.yaml
+
+```yaml
+- repo: https://github.com/kyicy/pre-commit-goreportcard
+  rev: master
+  hooks:
+    - id: goreportcard
+```
--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 
 	"github.com/gojp/goreportcard/check"
 )
@@ -15,10 +16,85 @@ var (
 	th      = flag.Float64("t", 90, "Threshold of failure command")
 )
 
+func run(dir string) (check.ChecksResult, error) {
+	filenames, skipped, err := check.GoFiles(dir)
+	if err != nil {
+		return check.ChecksResult{}, fmt.Errorf("could not get filenames: %v", err)
+	}
+	if len(filenames) == 0 {
+		return check.ChecksResult{}, fmt.Errorf("no .go files found")
+	}
+
+	err = check.RenameFiles(skipped)
+	if err != nil {
+		log.Println("Could not remove files:", err)
+	}
+	defer check.RevertFiles(skipped)
+
+	checks := []check.Check{
+		check.GoFmt{Dir: dir, Filenames: filenames},
+		check.GoVet{Dir: dir, Filenames: filenames},
+		check.GoLint{Dir: dir, Filenames: filenames},
+		check.GoCyclo{Dir: dir, Filenames: filenames},
+		// License{Dir: dir, Filenames: []string{}},
+		check.Misspell{Dir: dir, Filenames: filenames},
+		check.IneffAssign{Dir: dir, Filenames: filenames},
+		// ErrCheck{Dir: dir, Filenames: filenames}, // disable errcheck for now, too slow and not finalized
+	}
+
+	ch := make(chan check.Score)
+	for _, c := range checks {
+		go func(c check.Check) {
+			p, summaries, err := c.Percentage()
+			errMsg := ""
+			if err != nil {
+				log.Printf("ERROR: (%s) %v", c.Name(), err)
+				errMsg = err.Error()
+			}
+			s := check.Score{
+				Name:          c.Name(),
+				Description:   c.Description(),
+				FileSummaries: summaries,
+				Weight:        c.Weight(),
+				Percentage:    p,
+				Error:         errMsg,
+			}
+			ch <- s
+		}(c)
+	}
+
+	resp := check.ChecksResult{
+		Files: len(filenames),
+	}
+
+	var total, totalWeight float64
+	var issues = make(map[string]bool)
+	for i := 0; i < len(checks); i++ {
+		s := <-ch
+		resp.Checks = append(resp.Checks, s)
+		total += s.Percentage * s.Weight
+		totalWeight += s.Weight
+		for _, fs := range s.FileSummaries {
+			issues[fs.Filename] = true
+		}
+		if s.Error != "" {
+			resp.DidError = true
+		}
+	}
+	total /= totalWeight
+
+	sort.Sort(check.ByWeight(resp.Checks))
+	resp.Average = total
+	resp.Issues = len(issues)
+	resp.Grade = check.GradeFromPercentage(total * 100)
+
+	return resp, nil
+}
+
 func main() {
 	flag.Parse()
 
-	result, err := check.Run(*dir)
+	result, err := run(*dir)
 	if err != nil {
 		log.Fatalf("Fatal error checking %s: %s", *dir, err.Error())
 	}
